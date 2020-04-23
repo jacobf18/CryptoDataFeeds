9cd2d030554c6add1ae8879c1f2c8b4d08c5f1ee
jacob <j.e.feitelberg@gmail.com>
2020-04-22 23:57:16 -0700

    finished version 1 of coinbase pro websocket tick feed.

--- coinbasepro/getMinuteBTCUSD.js
+++ coinbasepro/getMinuteBTCUSD.js
@@ -4,15 +4,21 @@ const ws = new WebSocket('wss://ws-feed.pro.coinbase.com');
 const symbol = "BTC-USD";
 
 // Google cloud Pub/Sub
-const topicName = 'crypto-1m-data';
-const projectId = 'project-id'
+const topicName = 'tick-data-50';
+const projectId = 'crypto-intraday'
 const keyFilename = '../creds.json'
 const {PubSub} = require('@google-cloud/pubsub');
 const pubSubClient = new PubSub({projectId, keyFilename});
 
-async function publishMessage(data) {
-  const dataBuffer = Buffer.from(data);
-
+async function publishMessage(array, rawTime, volume) {
+  let open = array[0];
+  let high = Math.max.apply(Math, array);
+  let low = Math.min.apply(Math, array);
+  let close = array[array.length - 1];
+  let utcTime = Number(new Date(rawTime));
+  let data = {'time': utcTime, 'open': open, 'high': high, 'low': low, 'close': close, 'volume': volume};
+  //console.log(data);
+  const dataBuffer = Buffer.from(JSON.stringify(data));
   const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
   console.log(`Message ${messageId} published.`);
 }
@@ -23,6 +29,9 @@ ws.on('open', function open() {
 });
 
 let count = 0; /* Variable just for counting the heartbeats. */
+let volume = 0;
+let array = [];
+let rawTime = '';
 /* Parse a message as it is received */
 ws.on('message', function incoming(data) {
   /* If heartbeat, do nothing, but count */
@@ -30,16 +39,25 @@ ws.on('message', function incoming(data) {
   let type = dataJSON['type'];
 
   if (type == 'heartbeat') {
-    console.log('Heartbeat ' + count);
-    count++;
+    console.log('Heartbeat');
   }
-  else if (type == 'candles_1m_updates') {
-    console.log(dataJSON['changes']);
-    count = 0;
-    publishMessage(data).catch(console.error);
-  }
-  else {
-    console.log('Something else');
+  else if (type == 'match') {
+    let price = Number(dataJSON['price']);
+    console.log(price);
+    array[count] = price;
+    volume += Number(dataJSON['size']);
+    if (count == 0) {
+      rawTime = dataJSON['time'];
+      count++;
+    }
+    else if (count == 5) {
+      publishMessage(array, rawTime, volume).catch(console.error);
+      count = 0;
+      array = [];
+    }
+    else {
+      count++;
+    }
   }
 });
 
