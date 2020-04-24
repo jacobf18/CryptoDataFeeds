638f30cfa17de58f2be9a8df6e23f403cf48c878
jacob <j.e.feitelberg@gmail.com>
2020-04-23 18:27:43 -0700

    not complete yet, but almost converted to properly use queues instead of arrays

--- coinbasepro/getTicks.js
+++ coinbasepro/getTicks.js
@@ -10,14 +10,7 @@ const keyFilename = '../creds.json'
 const {PubSub} = require('@google-cloud/pubsub');
 const pubSubClient = new PubSub({projectId, keyFilename});
 
-async function publishMessage(array, rawTime, volume) {
-  let open = array[0];
-  let high = Math.max.apply(Math, array);
-  let low = Math.min.apply(Math, array);
-  let close = array[array.length - 1];
-  let utcTime = Number(new Date(rawTime));
-  let data = {'time': utcTime, 'open': open, 'high': high, 'low': low, 'close': close, 'volume': volume};
-  //console.log(data);
+async function publishMessage(data) {
   const dataBuffer = Buffer.from(JSON.stringify(data));
   const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
   console.log(`Message ${messageId} published.`);
@@ -28,10 +21,7 @@ ws.on('open', function open() {
   ws.send('{"type": "subscribe","product_ids": ["' + symbol + '"],"channels": ["matches"]}');
 });
 
-let count = 0; /* Variable just for counting the heartbeats. */
-let volume = 0;
-let array = [];
-let rawTime = '';
+let queue = [];
 /* Parse a message as it is received */
 ws.on('message', function incoming(data) {
   /* If heartbeat, do nothing, but count */
@@ -43,21 +33,11 @@ ws.on('message', function incoming(data) {
   }
   else if (type == 'match') {
     let price = Number(dataJSON['price']);
-    console.log(price);
-    array[count] = price;
-    volume += Number(dataJSON['size']);
-    if (count == 0) {
-      rawTime = dataJSON['time'];
-      count++;
-    }
-    else if (count == 5) {
-      publishMessage(array, rawTime, volume).catch(console.error);
-      count = 0;
-      array = [];
-    }
-    else {
-      count++;
-    }
+    let volume = Number(dataJSON['size']);
+    let utcTime = Number(new Date(dataJSON['time']));
+    let data = {'price': price, 'volume': volume, 'time': utcTime};
+    queue.push(data);
+    console.log(data);
   }
 });
 
@@ -71,3 +51,37 @@ wss.on('connection', function connection(ws) {
 
   ws.send('something');
 });
+
+let size = 5;
+async function checkQueue() {
+  while(true) {
+    if (queue.length >= size) {
+      // Create TOHLCV data
+      let prices = [];
+      let volume = 0;
+      let startTime = 0;
+      
+      let open = 0;
+      let close = 0;
+      for (let i = 0; i < size; i++) {
+        let d = queue.shift();
+        if (i == 0) {
+          startTime = d['time'];
+          open = d['price'];
+        }
+        else if (i == size - 1) {
+          close = d['price'];
+        }
+        prices.push(d['price']);
+        volume += d['volume'];
+      }
+      let high = Math.max(...prices);
+      let low = Math.min(...prices);
+      let data = {'time': startTime, 'open': open, 'high': high, 'low': low, 'close': close, 'volume': volume};
+      publishMessage(data);
+    }
+    // else, keep waiting
+  }
+}
+
+// checkQueue();
