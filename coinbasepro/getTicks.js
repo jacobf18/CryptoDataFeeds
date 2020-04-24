dba1277191a6bbc9051ebe65e44d57f99237d9a3
jacobf18 <j.e.feitelberg@gmail.com>
2020-04-24 00:54:08 -0700

    version 2 og coinbase getTicks.  Works with multiple workers now.

--- coinbasepro/getTicks.js
+++ coinbasepro/getTicks.js
@@ -1,66 +1,33 @@
-// Coinbae Pro Websocket
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://ws-feed.pro.coinbase.com');
-const symbol = "BTC-USD";
+// Node
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
-// Google cloud Pub/Sub
-const topicName = 'tick-data-50';
-const projectId = 'crypto-intraday'
-const keyFilename = '../creds.json'
-const {PubSub} = require('@google-cloud/pubsub');
-const pubSubClient = new PubSub({projectId, keyFilename});
+if (isMainThread) {
+  // Google cloud Pub/Sub
+  const topicName = 'tick-data-50';
+  const projectId = 'crypto-intraday';
+  const keyFilename = '../creds.json';
+  const {PubSub} = require('@google-cloud/pubsub');
+  const pubSubClient = new PubSub({projectId, keyFilename});
 
-async function publishMessage(data) {
-  const dataBuffer = Buffer.from(JSON.stringify(data));
-  const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
-  console.log(`Message ${messageId} published.`);
-}
-
-/* When the connection opens, send a subscription message. */
-ws.on('open', function open() {
-  ws.send('{"type": "subscribe","product_ids": ["' + symbol + '"],"channels": ["matches"]}');
-});
-
-let queue = [];
-/* Parse a message as it is received */
-ws.on('message', function incoming(data) {
-  /* If heartbeat, do nothing, but count */
-  let dataJSON = JSON.parse(data);
-  let type = dataJSON['type'];
-
-  if (type == 'heartbeat') {
-    console.log('Heartbeat');
+  async function publishMessage(data) {
+    const dataBuffer = Buffer.from(JSON.stringify(data));
+    const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
+    console.log(`Message ${messageId} published.`);
   }
-  else if (type == 'match') {
-    let price = Number(dataJSON['price']);
-    let volume = Number(dataJSON['size']);
-    let utcTime = Number(new Date(dataJSON['time']));
-    let data = {'price': price, 'volume': volume, 'time': utcTime};
-    queue.push(data);
-    console.log(data);
-  }
-});
 
-/* Start server */
-const wss = new WebSocket.Server({ port: 8080 });
+  const worker = new Worker(__filename);
+  let queue = [];
+  let size = 5;
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-});
-
-let size = 5;
-async function checkQueue() {
-  while(true) {
+  // This works synchronously so will not run again before the previous is done.
+  worker.on('message', (message) => {
+    queue.push(message);
     if (queue.length >= size) {
       // Create TOHLCV data
       let prices = [];
       let volume = 0;
       let startTime = 0;
-      
+      let symbol = queue[0]['symbol'];
       let open = 0;
       let close = 0;
       for (let i = 0; i < size; i++) {
@@ -75,13 +42,41 @@ async function checkQueue() {
         prices.push(d['price']);
         volume += d['volume'];
       }
+      console.log(queue.length);
       let high = Math.max(...prices);
       let low = Math.min(...prices);
-      let data = {'time': startTime, 'open': open, 'high': high, 'low': low, 'close': close, 'volume': volume};
+      let data = {'time': startTime, 'open': open, 'high': high, 'low': low, 'close': close, 'volume': volume, 'symbol': symbol};
+      console.log(data);
       publishMessage(data);
     }
-    // else, keep waiting
-  }
+  });
 }
+else {
+  // Coinbae Pro Websocket
+  const WebSocket = require('ws');
+  const ws = new WebSocket('wss://ws-feed.pro.coinbase.com');
+  const symbol = "BTC-USD";
 
-// checkQueue();
+  /* When the connection opens, send a subscription message. */
+  ws.on('open', function open() {
+    ws.send('{"type": "subscribe","product_ids": ["' + symbol + '"],"channels": ["matches"]}');
+  });
+
+  /* Parse a message as it is received */
+  ws.on('message', function incoming(data) {
+    /* If heartbeat, do nothing, but count */
+    let dataJSON = JSON.parse(data);
+    let type = dataJSON['type'];
+
+    if (type == 'heartbeat') {
+      console.log('Heartbeat');
+    }
+    else if (type == 'match') {
+      let price = Number(dataJSON['price']);
+      let volume = Number(dataJSON['size']);
+      let utcTime = Number(new Date(dataJSON['time']));
+      let data = {'price': price, 'volume': volume, 'time': utcTime, 'symbol': symbol};
+      parentPort.postMessage(data);
+    }
+  });
+}
